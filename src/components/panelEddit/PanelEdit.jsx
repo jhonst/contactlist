c69fa59595bfbe52870959e55436bc5ce1808dbd
Jhon Arteaga <jhon.arteaga@hotmail.com>
2022-10-14 00:27:22 -0500

    Implementing modular CSS

--- src/components/panelEddit/PanelEdit.jsx
+++ src/components/panelEddit/PanelEdit.jsx
@@ -1,4 +1,4 @@
-import "./PanelEdit.css";
+import styles from "./PanelEdit.module.css";
 import { useState } from "react";
 
 const PanelEdit = ({ changeEdit, addContact }) => {
@@ -25,16 +25,16 @@ const PanelEdit = ({ changeEdit, addContact }) => {
 
 
     return (
-        <div className="content-list">
-            <h1 className="title-principal">Add new contact</h1>
+        <div className={styles["content-list"]}>
+            <h1 className={styles["title-principal"]}>Add new contact</h1>
             <hr />
 
-            <form className="form-contact">
+            <form className={styles["form-contact"]}>
                 <label htmlFor="nameContact">Name: </label>
                 <input type="text" id="nameContact" name="nameContact" value={formNewContact.nameContact} onChange={handleEditForm} />
                 <label htmlFor="phoneContact">Phone: </label>
                 <input type="number" id="phoneContact" name="phoneContact" value={formNewContact.phoneContact} onChange={handleEditForm} />
-                <div className="container-radios">
+                <div className={styles["container-radios"]}>
                     <label htmlFor="genderContactM">Male: </label>
                     <input type="radio" name="genderContact" id="genderContactM" value={"M"} onChange={handleEditForm} />
                     <label htmlFor="genderContactF">Female: </label>
@@ -43,11 +43,11 @@ const PanelEdit = ({ changeEdit, addContact }) => {
 
             </form>
 
-            {!formReady && <h3 className="error">There are errors in the form!</h3>}
+            {!formReady && <h3 className={styles["error"]}>There are errors in the form!</h3>}
 
-            <div className="container-buttons">
-                <button className="button-list" onClick={validForm}>Done!</button>
-                <button className="button-list" onClick={changeEdit}>Cancel</button>
+            <div className={styles["container-buttons"]}>
+                <button className={styles["button-list"]} onClick={validForm}>Done!</button>
+                <button className={styles["button-list"]} onClick={changeEdit}>Cancel</button>
             </div>
 
         </div>
