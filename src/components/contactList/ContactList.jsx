2dd5979a6767d4f92b912ffc19a1afb19aeb8f2d
Jhon Arteaga <jhon.arteaga@hotmail.com>
2022-10-14 01:09:23 -0500

    responsive v1

--- src/components/contactList/ContactList.jsx
+++ src/components/contactList/ContactList.jsx
@@ -1,26 +1,26 @@
-import styles from"./ContactList.module.css"
+import styles from "./ContactList.module.css"
 
-const ContactList = ( { listCont, delUser } ) => {
+const ContactList = ({ listCont, delUser }) => {
 
-    return(
+    return (
         <div className={styles['contacts-container-master']}>
-            {listCont.map((user)=>(
+            {listCont.map((user) => (
                 <div className={styles['contacts-container']} key={user.id}>
                     <div className={styles['contact-image']}>
-                        <img className={styles['img-avatar']} src={user.gender === "M" ? "/images/avatarMasculino.jpg":"/images/avatarFemenino.jpg" } alt="sin Img" />
+                        <img className={styles['img-avatar']} src={user.gender === "M" ? "/images/avatarMasculino.jpg" : "/images/avatarFemenino.jpg"} alt="sin Img" />
                     </div>
                     <div className={styles['contact-data']}>
                         <p>{user.name}</p>
                         <p>{user.phone}</p>
                     </div>
                     <div className={styles['contact-buttons']}>
-                        <img className={styles['img-deletes']} src="/images/delete.png" alt="sin Img" onClick={()=>delUser(user.id, user.name)}/>
+                        <img className={styles['img-deletes']} src="/images/delete.png" alt="sin Img" onClick={() => delUser(user.id, user.name)} />
                     </div>
-                    </div>
+                </div>
 
             ))}
-            
-            
+
+
         </div>
     )
 }
